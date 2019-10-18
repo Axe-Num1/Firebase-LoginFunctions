42a856b855d88778de85b414424e6f51d6dcc457
KangMinseok <qbq5000@gmail.com>
2019-10-18 16:06:50 +0900

    First-Commit
    Start Project

---
+++ LoginFunction(firebase)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginFunction(firebase)
+//
+//  Created by 강민석 on 2019/10/18.
+//  Copyright © 2019 강민석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
